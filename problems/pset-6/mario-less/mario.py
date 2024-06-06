14aed3abd1562091bedeea051bff852c3116d0ab
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-06 15:33:02 -0300

    feat: submit pset 6 - mario less

---
+++ problems/pset-6/mario-less/mario.py
@@ -0,0 +1,24 @@
+def print_row(height: int, bricks: int) -> None:
+    print(" " * (height - bricks), end="")
+    print("#" * bricks)
+
+
+def read_height() -> int:
+    while True:
+        try:
+            height = int(input("Height: "))
+            if height >= 1 and height <= 8:
+                return height
+        except ValueError:
+            pass
+
+
+def main() -> None:
+    height = read_height()
+
+    for i in range(1, height + 1):
+        print_row(height, i)
+
+
+if __name__ == "__main__":
+    main()
